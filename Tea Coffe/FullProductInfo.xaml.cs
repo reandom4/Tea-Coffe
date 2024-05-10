255066860774bb10861ea5ccae72eb16d61a8105
reandom4 <vovic200966@gmail.com>
2024-05-10 14:01:58 +0300

    fullproductmenu

---
+++ Tea Coffe/FullProductInfo.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static Tea_Coffe.Window1;
+
+namespace Tea_Coffe
+{
+    /// <summary>
+    /// Логика взаимодействия для FullProductInfo.xaml
+    /// </summary>
+    public partial class FullProductInfo : Window
+    {
+        ProductItem productItem = new ProductItem();
+        public FullProductInfo(ProductItem item)
+        {
+            InitializeComponent();
+
+            try
+            {
+                productItem = item;
+                FullProductMenu.Visibility = Visibility.Visible;
+                name.Text = productItem.Name;
+                cost.Text = productItem.Cost.ToString();
+
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(productItem.ImageData);
+                bitmap.EndInit();
+
+                image.Source = bitmap;
+
+                Quantity.Content = productItem.Quantity;
+                unit.Content = productItem.Unit;
+
+                descriptionHead.Visibility = Visibility.Collapsed;
+                cooking_methodHead.Visibility = Visibility.Collapsed;
+                taste_and_aromaHead.Visibility = Visibility.Collapsed;
+                description.Visibility = Visibility.Collapsed;
+                cooking_method.Visibility = Visibility.Collapsed;
+                taste_and_aroma.Visibility = Visibility.Collapsed;
+                if (productItem.description != null && productItem.description != "")
+                {
+                    descriptionHead.Visibility = Visibility.Visible;
+                    description.Visibility = Visibility.Visible;
+                    description.Text = productItem.description;
+                }
+                if (productItem.cooking_method != null && productItem.cooking_method != "")
+                {
+                    cooking_methodHead.Visibility = Visibility.Visible;
+                    cooking_method.Visibility = Visibility.Visible;
+                    cooking_method.Text = productItem.cooking_method;
+                }
+                if (productItem.taste_and_aroma != null && productItem.taste_and_aroma != "")
+                {
+                    taste_and_aromaHead.Visibility = Visibility.Visible;
+                    taste_and_aroma.Visibility = Visibility.Visible;
+                    taste_and_aroma.Text = productItem.taste_and_aroma;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private async void PlusButton(object sender, MouseButtonEventArgs e)
+        {
+            if (productItem.Quantity + productItem.MinUnit > productItem.QuantityInStock)
+            {
+                MaxQuantity.Visibility = Visibility.Visible;
+                MaxQuantityText.Text = $"Сейчас в наличии только {productItem.Quantity}{productItem.Unit}";
+                await Task.Delay(3000);
+
+                MaxQuantity.Visibility = Visibility.Hidden;
+                return;
+            }
+            // Увеличиваем вес на 50 г
+            productItem.Quantity += productItem.MinUnit;
+            productItem.Cost += productItem.DefaultCost;
+            Quantity.Content = productItem.Quantity;
+            cost.Text = productItem.Cost.ToString();
+            
+            
+
+        }
+
+        private void MinusButton(object sender, MouseButtonEventArgs e)
+        {
+            if (productItem.Quantity == productItem.MinUnit)
+            {
+                return;
+            }
+            // Увеличиваем вес на 50 г
+            productItem.Quantity -= productItem.MinUnit;
+            productItem.Cost -= productItem.DefaultCost;
+            Quantity.Content = productItem.Quantity;
+            cost.Text = productItem.Cost.ToString();
+        }
+    }
+}
--- Tea Coffe/Window1.xaml.cs
+++ Tea Coffe/Window1.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -61,26 +62,326 @@ namespace Tea_Coffe
                 }
                 // Добавление объекта ProductItem в список
                 productList.Add(item);
-
+                item.description = row["description"].ToString();
+                item.cooking_method = row["cooking_method"].ToString();
+                item.taste_and_aroma = row["taste_and_aroma"].ToString();
+                item.MaxQuantity = "Collapsed";
             }
 
 
             ProductView.ItemsSource = productList;
         }
-        
 
+        void sort(string sorntvalue)
+        {
+
+        }
+        List<ProductItem> Basket = new List<ProductItem>();
+        private void InBasketButton(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ProductItem item = ((FrameworkElement)sender).DataContext as ProductItem;
+
+                var existingItem = Basket.FirstOrDefault(p => p.id == item.id);
+
+                if (existingItem != null)
+                {
+                    // Увеличиваем количество товара в корзине
+                    if (existingItem.BasketQuantity + item.Quantity > item.QuantityInStock)
+                    {
+                        item.BasketQuantity = item.QuantityInStock;
+                    }
+                    else
+                    {
+                        item.BasketQuantity += item.Quantity;
+                    }
+                    
+                }
+                else
+                {
+                    // Добавляем новый товар в корзину
+                    item.BasketQuantity = item.Quantity;
+                    Basket.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+       
+
+        FullProductInfo fullProductInfo;
+        private void OpenFullProductMenu(object sender, MouseButtonEventArgs e)
+        {
+            var item = ((FrameworkElement)sender).DataContext as ProductItem;
+
+            if (fullProductInfo != null)
+            {
+                fullProductInfo.Close();
+            }
+
+            fullProductInfo = new FullProductInfo(item);
+            fullProductInfo.Show();
+            fullProductInfo.Activate();
+            fullProductInfo.Focus();
+        }
+
+
+        //Анимации
         private void ListView_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
         {
             // Предотвращаем прокрутку колесиком мыши для ListView
             e.Handled = false;
         }
 
-        private void PlusButtonImage(object sender, MouseButtonEventArgs e)
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            // Перенаправляем событие колесика мыши из ScrollViewer в ListView
+            e.Handled = true;
+            var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta)
+            {
+                RoutedEvent = UIElement.MouseWheelEvent,
+                Source = sender
+            };
+            ProductView.RaiseEvent(eventArg);
+
+            //Закрытие меню при пролистывании страницы
+            if (leftPanel.Visibility == Visibility.Visible)
+            {
+                CloseLeftMenu();
+                return;
+            }
+        }
+        public class StringIsEmptyConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value is string str)
+                {
+                    return string.IsNullOrWhiteSpace(str);
+                }
+                return false;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+        private void ShowCatalogButton(object sender, RoutedEventArgs e)
+        {
+            if (leftPanel.Visibility == Visibility.Visible)
+            {
+                CloseLeftMenu();
+                return;
+            }
+            OpenLeftMenu();
+        }
+
+        private void leftPanelFonClick(object sender, MouseButtonEventArgs e)
+        {
+            CloseLeftMenu();
+        }
+
+        private void OpenLeftMenu()
+        {
+            //Анимация выдвигание левого меню
+            var PromotionLeftMenuanimation = new ThicknessAnimation
+            {
+                Duration = TimeSpan.FromSeconds(0.2), // Длительность анимации
+                From = new Thickness(-381, 0, 0, 0),            // Начальное положение
+                To = new Thickness(0, 0, 0, 0)     // Конечное положение (меню выдвигается)
+            };
+            //Анимация затемнения фона
+            var DimmingMenuanimation = new DoubleAnimation
+            {
+                From = 0.0,                         // Начальное значение прозрачности
+                To = 15.0,                           // Конечное значение прозрачности
+                Duration = TimeSpan.FromSeconds(2)  // Длительность анимации
+            };
+
+            
+            leftPanel.Visibility = Visibility.Visible;
+            // Запускаем анимацию
+            leftPanelFon.BeginAnimation(UIElement.OpacityProperty, DimmingMenuanimation);
+            leftMenuPanel.BeginAnimation(MarginProperty, PromotionLeftMenuanimation);
+        }
+        private void CloseLeftMenu()
+        {
+            if (leftPanel.Visibility == Visibility.Collapsed)
+            {               
+                return;
+            }
+            //Анимация выдвигание левого меню
+            var PromotionLeftMenuanimation = new ThicknessAnimation
+            {
+                Duration = TimeSpan.FromSeconds(0.2), // Длительность анимации
+                From = new Thickness(0, 0, 0, 0),            // Начальное положение
+                To = new Thickness(-381, 0, 0, 0)     // Конечное положение (меню выдвигается)
+            };
+            //Анимация затемнения фона
+            var DimmingMenuanimation = new DoubleAnimation
+            {
+                From = 1.0,                         // Начальное значение прозрачности
+                To = 0.0,                           // Конечное значение прозрачности
+                Duration = TimeSpan.FromSeconds(0.2)  // Длительность анимации
+
+            };
+            RightMenuCocoaPanel.Visibility = Visibility.Collapsed;
+            RightMenuTeaPanel.Visibility = Visibility.Collapsed;
+            RightMenuCoffePanel.Visibility = Visibility.Collapsed;
+            TeaTB.Foreground = Brushes.Black;
+            CoffeTB.Foreground = Brushes.Black;
+            CocoaTB.Foreground = Brushes.Black;
+            DimmingMenuanimation.Completed += (sender, e) =>
+            {
+                // Скрываем элемент leftPanel после завершения анимации
+                leftPanel.Visibility = Visibility.Collapsed;
+            };
+            // Запускаем анимацию
+            leftPanelFon.BeginAnimation(UIElement.OpacityProperty, DimmingMenuanimation);
+            leftMenuPanel.BeginAnimation(MarginProperty, PromotionLeftMenuanimation);
+            
+        }
+
+        private void TextBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            CloseLeftMenu();
+        }
+
+        private void Hover_TeaTB(object sender, MouseEventArgs e)
+        {
+            TeaTB.Foreground = new SolidColorBrush(Color.FromArgb(0xFF, 0x2F, 0xAC, 0x66));
+            RightMenuTeaPanel.Visibility = Visibility.Visible;
+
+            RightMenuCoffePanel.Visibility = Visibility.Collapsed;
+            RightMenuCocoaPanel.Visibility = Visibility.Collapsed;
+            CoffeTB.Foreground = Brushes.Black;
+            CocoaTB.Foreground = Brushes.Black;
+        }
+        private void Hover_CoffeTB(object sender, MouseEventArgs e)
+        {
+            CoffeTB.Foreground = new SolidColorBrush(Color.FromArgb(0xFF, 0x2F, 0xAC, 0x66));
+            RightMenuCoffePanel.Visibility = Visibility.Visible;
+
+            RightMenuTeaPanel.Visibility = Visibility.Collapsed;
+            RightMenuCocoaPanel.Visibility = Visibility.Collapsed;
+            TeaTB.Foreground = Brushes.Black;
+            CocoaTB.Foreground = Brushes.Black;
+        }
+        private void Hover_CocoaTB(object sender, MouseEventArgs e)
+        {
+            CocoaTB.Foreground = new SolidColorBrush(Color.FromArgb(0xFF, 0x2F, 0xAC, 0x66));
+            RightMenuCocoaPanel.Visibility = Visibility.Visible;
+
+            RightMenuTeaPanel.Visibility = Visibility.Collapsed;
+            RightMenuCoffePanel.Visibility = Visibility.Collapsed;
+            TeaTB.Foreground = Brushes.Black;
+            CoffeTB.Foreground = Brushes.Black;
+        }
+
+        private void Panel_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender is TextBlock)
+            {
+                TextBlock textBlock = sender as TextBlock;
+                textBlock.Foreground = new SolidColorBrush(Color.FromArgb(0xFF, 0x2F, 0xAC, 0x66));
+            }
+        }
+        private void Panel_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender is TextBlock)
+            {
+                TextBlock textBlock = sender as TextBlock;
+                textBlock.Foreground = Brushes.Black;
+            }
+
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            BottomSortPanel1.Visibility = Visibility.Visible;
+        }
+
+        private void Grid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            BottomSortPanel1.Visibility = Visibility.Collapsed;
+        }
+
+        private void sortTB_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if(sender is Border)
+            {
+                Border border =  sender as Border;
+                border.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0xED, 0xED, 0xED));
+                
+            }
+        }
+
+        private void sortTB_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender is Border)
+            {
+                
+                Border border = sender as Border;
+                TextBlock tb = border.Child as TextBlock;
+                if(tb.Text == mainsortTB.Text)
+                {
+                    return;
+                }
+                border.Background = Brushes.White;
+            }
+
+        }
+
+        private void sortTB_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Border)
+            {
+                Border border = sender as Border;
+                TextBlock tb = border.Child as TextBlock;
+                if (tb.Text == mainsortTB.Text)
+                {
+                    return;
+                }
+                mainsortTB.Text = tb.Text;
+                
+                sort(tb.Text);
+                if(border.Name == "popularTB")
+                {
+                    expensiveTB.Background = Brushes.White; ;
+                    cheapTB.Background = Brushes.White;
+                }
+                if (border.Name == "expensiveTB")
+                {
+                    popularTB.Background = Brushes.White; ;
+                    cheapTB.Background = Brushes.White;
+                }
+                if (border.Name == "cheapTB")
+                {
+                    expensiveTB.Background = Brushes.White; ;
+                    popularTB.Background = Brushes.White;
+
+                }
+                BottomSortPanel1.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private async void PlusButtonImageAsync(object sender, MouseButtonEventArgs e)
         {
             var item = ((FrameworkElement)sender).DataContext as ProductItem; // Замените YourItemType на ваш тип данных
 
-            if(item.Quantity + item.MinUnit > item.QuantityInStock)
+            if (item.Quantity + item.MinUnit > item.QuantityInStock)
             {
+                item.MaxQuantity = "Visible";
+
+                await Task.Delay(3000);
+
+                item.MaxQuantity = "Hidden";
                 return;
             }
             // Увеличиваем вес на 50 г
@@ -101,6 +402,24 @@ namespace Tea_Coffe
         }
 
 
+
+
+        public class BasketItem
+        {
+            public int id { get; set; }
+            public string Name { get; set; }
+            public string ImageData { get; set; }
+
+            public int DefaultCost { get; set; }
+            public string Category { get; set; }
+            public string Unit { get; set; }
+            public int MinUnit { get; set; }
+            public int QuantityInStock { get; set; }
+            private int Quantity { get; set; }
+            public int Cost { get; set; }
+        }
+
+        //конструктор ProductItem
         public class ProductItem : INotifyPropertyChanged
         {
             public int id { get; set; }
@@ -145,40 +464,25 @@ namespace Tea_Coffe
             {
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
             }
+            public string description { get; set; }
+            public string cooking_method { get; set; }
+            public string taste_and_aroma { get; set; }
+            private string maxQuantity;
 
-        }
-
-        private void InBasketButton(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            // Перенаправляем событие колесика мыши из ScrollViewer в ListView
-            e.Handled = true;
-            var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta)
+            public string MaxQuantity
             {
-                RoutedEvent = UIElement.MouseWheelEvent,
-                Source = sender
-            };
-            ProductView.RaiseEvent(eventArg);
-        }
-        public class StringIsEmptyConverter : IValueConverter
-        {
-            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            {
-                if (value is string str)
+                get { return maxQuantity; }
+                set
                 {
-                    return string.IsNullOrWhiteSpace(str);
+                    if (maxQuantity != value)
+                    {
+                        maxQuantity = value;
+                        OnPropertyChanged(nameof(MaxQuantity));
+                    }
                 }
-                return false;
             }
 
-            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-            {
-                throw new NotImplementedException();
-            }
+            public int BasketQuantity { get; set; }
         }
     }
 }
